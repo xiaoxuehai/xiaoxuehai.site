fcc3a57d5d8982eb1c712ab1a08e720969ddbb42
xiaoxuehai <1379228273@qq.com>
2024-04-06 22:26:11 +0800

    chore: 测试页面

--- src/app/testpage/page.tsx
+++ src/app/testpage/page.tsx
@@ -1,10 +1,10 @@
 'use client';
+
 import Image from 'next/image';
 
 import { Button } from '@/components/ui/button';
 import { useToast } from '@/components/ui/use-toast';
-
-export default function Home() {
+export default function Index() {
   const { toast } = useToast();
 
   return (
